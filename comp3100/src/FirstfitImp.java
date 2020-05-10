3484e8098ee6c8a196fb6f2d723ff53e79739185
vythu sothy <62238024+vythusothy@users.noreply.github.com>
2020-05-10 19:46:20 +1000

    Update FirstfitImp.java

--- comp3100/src/FirstfitImp.java
+++ comp3100/src/FirstfitImp.java
@@ -35,3 +35,15 @@ class Firstfit
                   }
 
 }
+ public static void main(String[] args){
+
+     int blockSize[] = {100, 500, 200, 300, 600};
+     int processSize[] = {212, 417, 112, 426};
+     int p = blockSize.length;
+     int s = processSize.length;
+     
+     firstFit(blockSize, p, processSize, s);
+
+}
+
+}
